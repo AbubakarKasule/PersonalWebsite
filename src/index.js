80aa539b1835aad3515311c95e7778c8a44f7c60
Abubakar <abubakar.i.kasule.21@dartmouth.edu>
2020-04-17 00:02:30 -0400

    SA3 submission

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('starting up!');
+// change require to es6 import style
+import $ from 'jquery';
+import './style.scss';
+
+let num = 0;
+const output = () => {
+  $('#main').html(`You've been on this page for ${num} seconds.`);
+  num += 1;
+};
+
+setInterval(output, 1000);
